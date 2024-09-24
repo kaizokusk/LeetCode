06c17b21f69843be0771948370d7109f9aa188d0
kaizokusk <sai.snist.2019@gmail.com>
2024-09-24 17:41:43 +0530

    :test_tube:checking revert so,it is faulted one

--- cpp/0001_two_sum.C
+++ cpp/0001_two_sum.C
@@ -4,8 +4,7 @@ public:
         for(int i=0;i<nums.size();i++){
             for(int j=0;j<nums.size();j++){
                 if(i!=j){
-                    if(nums[i]+nums[j]==target){
-                        return {i,j};
+                    
                     }
                 }
 
